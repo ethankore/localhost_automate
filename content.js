a50b283a9ca7f79c1f79cc94a5ea07b28e13fadd
stevensmann <steven.s.mann@gmail.com>
2013-10-29 15:14:36 +0000

    Update content.js

--- content.js
+++ content.js
@@ -6,7 +6,7 @@ chrome.storage.local.get('localhost_path', function(result){
 	var currenthref = window.location.href;
 
 	if (protocol == 'file:' && currenthref.indexOf(localPath) > -1 ) {
-			var newhref = currenthref.replace(localPath, 'localhost');
+			var newhref = currenthref.replace(localPath, 'localhost/');
 			newhref = newhref.replace('file:///', 'http://')
 			window.location.href = newhref;
 	}	
