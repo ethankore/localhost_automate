7360ced27dab5088ffabcde0dab99772b863d921
Ardethian <ardethian@gmail.com>
2013-11-01 03:08:18 +0200

    Added indicator for file URLs access authorization

--- background.js
+++ background.js
@@ -1,5 +1,7 @@
 chrome.extension.isAllowedFileSchemeAccess(function(result){
 	if (!result) {
 		chrome.browserAction.setBadgeText({text: 'x'});
+	} else {
+		chrome.browserAction.setBadgeText({text: ''});
 	}
 });
